5e34bdec53fc7a9f289f265a8709f8017d900e30
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-22 21:55:32 +0100

    Add test and laxing jscs

---
+++ test/devices.js
@@ -0,0 +1,72 @@
+/*jshint undef:false */
+var RSVP = require('rsvp'),
+    chai = require('chai');
+chai.should();
+
+var devicesResult = {
+    device: [
+        {
+            id: '240000',
+            clientDeviceId: '14',
+            name: '##Everything',
+            state: 0,
+            statevalue: null,
+            methods: 0,
+            type: 'group',
+            client: '10000',
+            clientName: 'home',
+            online: '1',
+            editable: 1,
+            devices: '240001, 240001'
+        },
+        {
+            id: '240001',
+            clientDeviceId: '7',
+            name: 'Livingroom-light',
+            state: 0,
+            statevalue: '0',
+            methods: 0,
+            type: 'device',
+            client: '10000',
+            clientName: 'home',
+            online: '1',
+            editable: 1
+        },
+        {
+            id: '240002',
+            clientDeviceId: '7',
+            name: 'Livingroom-light',
+            state: 0,
+            statevalue: '0',
+            methods: 0,
+            type: 'device',
+            client: '10000',
+            clientName: 'home',
+            online: '1',
+            editable: 1
+        }]
+};
+
+describe('devices', function () {
+    describe('#getDevices()', function () {
+        var logger = console;
+        describe('success', function () {
+            it('returns an array of sensors - if everything works fine', function (done) {
+                var client = {
+                        invoke: function invoke () {
+                            return new RSVP.Promise(function (resolve) {
+                                resolve(devicesResult);
+                            });
+                        }
+                    },
+                    devices = require('../lib').Devices(client, {logger: logger});
+                devices.getDevices().then(function (result) {
+                    result.should.equal(devicesResult.device);
+                    done();
+                }).catch(function (reason) {
+                    done(reason);
+                });
+            });
+        });
+    });
+});
