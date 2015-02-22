8935b4fa58b084e99f5a541326152c62cb57065a
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-22 21:35:00 +0100

    Introducing jscs

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
 var gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
-	mocha = require('gulp-mocha'),
-	cover = require('gulp-coverage'),
-	jscs = require('gulp-jscs');
+  jshint = require('gulp-jshint'),
+  mocha = require('gulp-mocha'),
+  cover = require('gulp-coverage'),
+  jscs = require('gulp-jscs');
 
-gulp.task('default', ['lint', 'test'], function () {
+gulp.task('default', ['jscs', 'lint', 'test'], function () {
 });
 
 gulp.task('lint', function () {
-	return gulp.src(['./lib/*.js', './test/*.js'])
-		.pipe(jshint())
-		.pipe(jshint.reporter('default', {verbose: true}));
+  return gulp.src(['./lib/*.js', './test/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', {verbose: true}));
 });
 
 gulp.task('test', function () {
-	return gulp.src('./test', {read: false})
-		.pipe(cover.instrument({
-			pattern: ['*lib/*.js'],
-			debugDirectory: 'debug'
-		}))
-		.pipe(mocha({reporter: 'nyan'}))
-		.pipe(cover.gather())
-		.pipe(cover.format())
-		.pipe(gulp.dest('reports'));
+  return gulp.src('./test', {read: false})
+    .pipe(cover.instrument({
+      pattern: ['*lib/*.js'],
+      debugDirectory: 'debug'
+    }))
+    .pipe(mocha({reporter: 'nyan'}))
+    .pipe(cover.gather())
+    .pipe(cover.format())
+    .pipe(gulp.dest('reports'));
+});
+
+gulp.task('jscs', function () {
+  return gulp.src(['./lib/*.js', './test/*.js'])
+    .pipe(jscs());
 });
